a9c1550e1f0166cbcbb0eb0229c8a82b915ee860
arthudearaujoneves3 <arthurdearaujoneves333@gmail.com>
2025-05-05 16:57:55 -0300

    Aula 6: Estilos globais com Styled-Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button color="primary"/>
+      <GlobalStyle />
+
       <Button color="secondary"/>
+      <Button color="primary"/>
       <Button color="danger"/>
       <Button />
     </ThemeProvider>
